2aefe7f945f73b5ea1de083e39c8b459a057263f
LoZioo <60042676+LoZioo@users.noreply.github.com>
2023-02-06 13:33:09 +0100

    feat: added DAC sinewave generator.

--- src/Fonometro/src/main.cpp
+++ src/Fonometro/src/main.cpp
@@ -5,7 +5,13 @@
 #include <const.h>
 
 // Setup routines.
-inline void setup_wifi(), spawn_threads();
+inline void setup_wifi(), spawn_threads(), setup_timers();
+
+// ISR.
+void IRAM_ATTR timer0_OVF_ISR();
+
+// Hardware timers.
+hw_timer_t *timer0;
 
 // Threads and IPC.
 void thread_1(void*);
@@ -16,24 +22,22 @@ void setup(){
 	Serial.begin(115200);
 
 	// setup_wifi();
-	spawn_threads();
+	setup_timers();
+	// spawn_threads();
 }
 
 void thread_1(void *parameters){
-	// f = 1 / (SAMPLES_LEN * DELAY)
-	// f = 1 / (50 * 0.001)
+	// const uint16_t SAMPLES_LEN = 10;
+	// const uint8_t SAMPLES[] = { 100, 158, 195, 195, 158, 100, 41, 4, 4, 41 };
 
-	const uint16_t SAMPLES_LEN = 50;
-	const uint8_t SAMPLES[] = { 100, 112, 124, 136, 148, 158, 168, 177, 184, 190, 195, 198, 199, 199, 198, 195, 190, 184, 177, 168, 158, 148, 136, 124, 112, 100, 87, 75, 63, 51, 41, 31, 22, 15, 9, 4, 1, 0, 0, 1, 4, 9, 15, 22, 31, 41, 51, 63, 75, 87 };
+	// while(true){
+	// 	for(int i=0; i<SAMPLES_LEN; i++){
+	// 		dacWrite(DAC1, SAMPLES[i]);
+	// 		// delayMicroseconds(20);
 
-	while(true){
-		for(int i=0; i<SAMPLES_LEN; i++){
-			dacWrite(DAC1, SAMPLES[i]);
-			delayMicroseconds(100);
-
-			// vTaskDelay(1 / portTICK_PERIOD_MS);
-		}
-	}
+	// 		vTaskDelay(1 / portTICK_PERIOD_MS);
+	// 	}
+	// }
 }
 
 inline void setup_wifi(){
@@ -61,4 +65,26 @@ inline void spawn_threads(){
 
 	// Deleting the spawner thread (setup thread).
 	vTaskDelete(NULL);
+}
+
+inline void setup_timers(){
+	timer0 = timerBegin(0, PRESCALER, true);
+	timerAlarmWrite(timer0, AUTORELOAD, true);
+	timerAttachInterrupt(timer0, &timer0_OVF_ISR, true);
+
+	timerWrite(timer0, 0);
+	timerAlarmEnable(timer0);
+}
+
+// Sinewave generator.
+volatile uint16_t k = 0;
+
+const uint16_t SAMPLES_LEN = 40;
+const uint8_t SAMPLES[] = { 127, 138, 150, 161, 171, 180, 187, 193, 198, 201, 202, 201, 198, 193, 187, 180, 171, 161, 150, 138, 127, 115, 103, 92, 82, 73, 66, 60, 55, 52, 52, 52, 55, 60, 66, 73, 82, 92, 103, 115 };
+
+void IRAM_ATTR timer0_OVF_ISR(){
+	if(k == SAMPLES_LEN)
+		k = 0;
+
+	dacWrite(DAC1, SAMPLES[k++]);
 }
