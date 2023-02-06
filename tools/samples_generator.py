a5883525652fc929c46597b5424a80e7cd193cd0
LoZioo <60042676+LoZioo@users.noreply.github.com>
2023-02-06 13:32:56 +0100

    fix

--- tools/samples_generator.py
+++ tools/samples_generator.py
@@ -1,21 +1,26 @@
 import sys, math
 import matplotlib.pyplot as plt
 
-# Usage: python samples_generator.py AMP SAMPLES_NUM
+# Usage: python samples_generator.py SAMPLES_NUM AMP
 def main() -> None:
 	assert len(sys.argv) == 3
 
 	try:
-		AMP = int(sys.argv[1])
-		SAMPLES_NUM = int(sys.argv[2])
+		SAMPLES_NUM = int(sys.argv[1])
+		AMP = int(sys.argv[2])
 	
 	except:
 		sys.exit("Parameter errors.")
 
-	samples = generate_samples(AMP, SAMPLES_NUM)
+	samples = generate_samples(SAMPLES_NUM, AMP)
 
-	print("Min: %d" % min(samples))
-	print("Max: %d" % max(samples))
+	AMP_MIN = min(samples)
+	AMP_MAX = max(samples)
+	AMP_PP = AMP_MAX - AMP_MIN
+
+	print("Min: %d" % AMP_MIN)
+	print("Max: %d" % AMP_MAX)
+	print("Amp-PP: %d" % AMP_PP)
 
 	print()
 	print("const uint16_t SAMPLES_LEN = %d;" % len(samples))
@@ -29,16 +34,16 @@ def main() -> None:
 	# plt.show()
 
 # Sample at the given sampling frequency, one period from a sinewave with the given amp.
-def generate_samples(amp: int, samples_num: int) -> list[int]:
+def generate_samples(samples_num: int, amp: int) -> list[int]:
 	samples: list[int] = []
 
 	PERIOD_US = 1000000
 	SAMPLING_PERIOD_US = int(PERIOD_US / samples_num)
 
-	DC_SHIFT = amp/2
+	DC_SHIFT = 127
 	
 	for x in range(0, PERIOD_US, SAMPLING_PERIOD_US):
-		samples.append(int(DC_SHIFT + DC_SHIFT * math.sin(2 * math.pi * x/PERIOD_US)))
+		samples.append(int(DC_SHIFT + amp/2 * math.sin(2 * math.pi * x/PERIOD_US)))
 
 	return samples
 
