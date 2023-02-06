d5e47e8d47b6fefafac66166cb8ce45bc75201fe
LoZioo <60042676+LoZioo@users.noreply.github.com>
2023-02-06 13:36:13 +0100

    feat: added MIC GPIOs.

--- src/Fonometro/include/const.h
+++ src/Fonometro/include/const.h
@@ -4,6 +4,13 @@
 		Flash:	4MB
 */
 
+// GPIOs.
+#define MIC_SUM	32
+#define MIC_1		36
+#define MIC_2		39
+#define MIC_3		34
+#define MIC_4		35
+
 // Protocol CPU and app CPU.
 #define PRO_CPU 0
 #define APP_CPU 1
