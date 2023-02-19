edeee3ef15696cba906b86ef020236432bab3e15
LoZioo <60042676+LoZioo@users.noreply.github.com>
2023-02-19 11:58:07 +0100

    feat: removed manual sampling parameters.

--- src/Fonometro/include/const.h
+++ src/Fonometro/include/const.h
@@ -1,6 +1,6 @@
 /*
 	ESP32-D0WD-V3 (revision v3.0) (CH9102X):
-		RAM:		?? 327.680 bytes
+		RAM:		320kB
 		Flash:	4MB
 		Timers:	
 			16 bit prescaler.
@@ -17,20 +17,3 @@
 // Protocol CPU and app CPU.
 #define PRO_CPU 0
 #define APP_CPU 1
-
-// Sampling timer parameters (16kHz).
-#define TIMER0_PRE	5000		// 80.000.000Hz / 5000 = 16.000Hz
-#define TIMER0_ARR	1
-
-// The RAM requirements are: 2 byte/sample * SAMPLING_FREQ * SAMPLING_DURATION_S = 32kB
-#define SAMPLING_FREQ					16000
-#define SAMPLING_DURATION_S		1
-#define SAMPLES_LEN						SAMPLING_FREQ * SAMPLING_DURATION_S
-
-// Sinewave timer parameters for the signal generator (DAC).
-#define TIMER1_PRE	2000		// 80.000.000Hz / 2000 = 40.000Hz = 40 * 1.000Hz
-#define TIMER1_ARR	1
-
-// Sinewave samples.
-#define DAC_SAMPLES_LEN 40
-const uint8_t DAC_SAMPLES[] = { 127, 138, 150, 161, 171, 180, 187, 193, 198, 201, 202, 201, 198, 193, 187, 180, 171, 161, 150, 138, 127, 115, 103, 92, 82, 73, 66, 60, 55, 52, 52, 52, 55, 60, 66, 73, 82, 92, 103, 115 };
