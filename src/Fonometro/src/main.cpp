5219ac3dc8d0acedead0a4cf8244f1f3a504298b
LoZioo <60042676+LoZioo@users.noreply.github.com>
2023-02-22 18:43:52 +0100

    feat: implemented Knuth running mean algorithm.

--- src/Fonometro/src/main.cpp
+++ src/Fonometro/src/main.cpp
@@ -33,22 +33,23 @@ void sample_thread(void *parameters){
 	int16_t *samples = new int16_t[SAMPLES_TO_READ];
 	uint32_t samples_len;
 
+	// Knuth running mean (2047 is the expected DC value).
+	float dc_offset = 2047;
+
 	while(true){
 		// Read samples [0, 4096].
 		samples_len = read_adc_i2s_samples(samples, SAMPLES_TO_READ);
 
-		uint64_t dc_sum = 0;
+		float adc_voltage_rms = 0;
 		for(int i=0; i<samples_len; i++){
 			// Extract the 12 lower bits.
 			samples[i] &= 0x0FFF;
-			dc_sum += samples[i];
-		}
 
-		uint16_t dc_offset = dc_sum / samples_len;
-		float adc_voltage_rms = 0;
+			// Running mean.
+			dc_offset = dc_offset + ((float) samples[i] - dc_offset) / samples_len;
 
-		for(int i=0; i<samples_len; i++){
-			float adc_voltage = (samples[i] - dc_offset) * ADC_VAL_TO_VOLTS;
+			// Data extraction.
+			float adc_voltage = (samples[i] - round(dc_offset)) * ADC_VAL_TO_VOLTS;
 			float mic_voltage = adc_voltage / PREAMP_GAIN;
 
 			// Needed for the FFT.
@@ -64,7 +65,8 @@ void sample_thread(void *parameters){
 		float mic_voltage_rms = adc_voltage_rms / PREAMP_GAIN;
 		int16_t db_spl = round(20 * log10(mic_voltage_rms / MIC_DB_SPL_CONVERSION));
 
-		Serial.printf("dc_offset: %d\n", dc_offset);
+		Serial.printf("samples_len: %d\n", samples_len);
+		Serial.printf("dc_offset: %f\n", dc_offset);
 		Serial.printf("adc_voltage_rms_mV: %f\n", adc_voltage_rms * 1000);
 		Serial.printf("mic_voltage_rms_mV: %f\n", mic_voltage_rms * 1000);
 		Serial.printf("db_spl: %d\n", db_spl);
