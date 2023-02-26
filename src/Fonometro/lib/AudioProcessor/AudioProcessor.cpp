f6544f2015cca71310cecbea1670f80844da888d
LoZioo <60042676+LoZioo@users.noreply.github.com>
2023-02-26 13:19:18 +0100

    feat: beginning class translation.

---
+++ src/Fonometro/lib/AudioProcessor/AudioProcessor.cpp
@@ -0,0 +1,10 @@
+/**
+ * @file AudioProcessor.cpp
+ * @author Davide Scalisi
+ * @brief class to acquire, analyze and extract the main parameters from an audio signal.
+ * @version 0.1
+ * @date 2023-02-26
+ * 
+ * @copyright Copyright (c) 2023 Davide Scalisi
+ * 
+ */
---
+++ src/Fonometro/lib/AudioProcessor/AudioProcessor.h
@@ -0,0 +1,13 @@
+/**
+ * @file AudioProcessor.h
+ * @author Davide Scalisi
+ * @brief class to acquire, analyze and extract the main parameters from an audio signal.
+ * @version 0.1
+ * @date 2023-02-26
+ * 
+ * @copyright Copyright (c) 2023 Davide Scalisi
+ * 
+ */
+
+#pragma once
+#include <Arduino.h>
