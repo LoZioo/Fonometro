b0a7210ef85d06aad25cb7df8e07e009438eb169
LoZioo <60042676+LoZioo@users.noreply.github.com>
2023-02-26 16:21:14 +0100

    feat: first working conversion to class.

--- src/Fonometro/include/const.h
+++ src/Fonometro/include/const.h
@@ -18,44 +18,25 @@
 #define PRO_CPU 0
 #define APP_CPU 1
 
-// Conversion factors.
-#define ADC_VAL_TO_VOLTS	8.056640625e-4	// 3.3V / 4096
-#define PREAMP_GAIN				9.2
+// Main thread delay.
+#define SENDING_DATA_PERIOD_MS	500
 
 // Mic sensitivity (https://www.dsprelated.com/showthread/audiodsp/1153-1.php).
 // Theoretical values.
 // #define MIC_SENSITIVITY_DB			-50											// db(Vrms/Pa)
-// #define MIC_SENSITIVITY				3.1622776601683793e-3		// pow(10, MIC_SENSITIVITY_DB / 20)			// Vrms/Pa.
+// #define MIC_SENSITIVITY				3.1622776601683793e-3		// pow(10, MIC_SENSITIVITY_DB / 20)			// Vrms/Pascal.
 // #define MIN_SOUND_SPL_THR			2e-5										// 20 uPascal
-// #define MIC_DB_SPL_CONVERSION	6.3245553203367586e-8		// MIC_SENSITIVITY * MIN_SOUND_SPL_THR
+// #define MIC_SPL_CONVERSION			6.3245553203367586e-8		// MIC_SENSITIVITY * MIN_SOUND_SPL_THR
 
-// Empyric value: you have to solve "20 * log10(READ_MIC_VOLTAGE_RMS / x) = REAL_DB_VALUE (from another audio meter)".
-#define MIC_DB_SPL_CONVERSION	5.26799e-6		// MIC_SENSITIVITY * MIN_SOUND_SPL_THR
+AudioProcessor_init_t AudioProcessor_init {
+	.i2s_unit =			I2S_NUM_0,
+	.adc_unit =			ADC_UNIT_1,
+	.adc_channel =	ADC1_CHANNEL_4,		// MIC_SUM, GPIO32.
 
-// I2S and ADC configurations.
-#define I2S_UNIT					I2S_NUM_0
+	.sample_rate =			16000,				// Be careful: RAM isn't ever enough.
+	.sampling_time_ms =	1000,
 
-#define ADC_UNIT					ADC_UNIT_1
-#define ADC_CHANNEL				ADC1_CHANNEL_4		// MIC_SUM, GPIO32.
-#define ADC_SAMPLE_RATE		16000							// Be careful: RAM isn't ever enough.
-#define ADC_DEPTH					I2S_BITS_PER_SAMPLE_32BIT
-
-// Sampling configurations.
-#define	REC_TIME_SEC						1		// Be careful: RAM isn't ever enough.
-#define SAMPLES_TO_READ					(uint32_t) ceil(REC_TIME_SEC * ADC_SAMPLE_RATE)
-#define SENDING_DATA_PERIOD_MS	1
-
-// I2S configurations.
-i2s_config_t adc_i2s_config = {
-	.mode = (i2s_mode_t)(I2S_MODE_MASTER | I2S_MODE_RX | I2S_MODE_ADC_BUILT_IN),
-	.sample_rate = ADC_SAMPLE_RATE,
-	.bits_per_sample = ADC_DEPTH,
-	.channel_format = I2S_CHANNEL_FMT_ONLY_LEFT,
-	.communication_format = I2S_COMM_FORMAT_STAND_MSB,
-	.intr_alloc_flags = ESP_INTR_FLAG_LEVEL1,
-	.dma_buf_count = 4,
-	.dma_buf_len = 1024,
-	.use_apll = false,
-	.tx_desc_auto_clear = false,
-	.fixed_mclk = 0
+	.adc_val_to_volts =		8.056640625e-4,		// 3.3V / 4096
+	.preamp_gain =				9.2,
+	.mic_spl_conversion =	5.26799e-6	// Empyric value: you have to solve "20 * log10(READ_MIC_VOLTAGE_RMS / x) = REAL_DB_VALUE (from another audio meter)".
 };
--- src/Fonometro/src/main.cpp
+++ src/Fonometro/src/main.cpp
@@ -1,8 +1,8 @@
 #include <Arduino.h>
 #include <WiFi.h>
 
-#include <driver/i2s.h>
 // #include <fft.h>
+#include <AudioProcessor.h>
 
 #include <password.h>
 #include <const.h>
@@ -13,74 +13,41 @@ TaskHandle_t sample_thread_handle, main_thread_handle;
 
 #include <proc.h>
 
+AudioProcessor sound(AudioProcessor_init);
+
 void loop(){}
 void setup(){
 	Serial.begin(115200);
+	sound.begin();
 
 	setup_gpio();
 	// setup_wifi();
 
-	setup_adc_i2s();
 	spawn_threads();
 }
 
 void sample_thread(void *parameters){
 	Serial.println("sample_thread");
-	
-	// Range [-2048, 2047].
-	int32_t *buff32_int = new int32_t[SAMPLES_TO_READ];							// Intended to save samples. 
-	float *buff32_float = (float*) buff32_int;											// Intended to save SPL values.
-	// Use the same allocated memory, but use it as float memory.
-
-	uint32_t buff32_len;
-
-	// Knuth running mean (2047 is the expected DC value).
-	float dc_offset = 2047;
 
 	while(true){
-		// Read samples [0, 4096].
-		buff32_len = read_adc_i2s_samples(buff32_int, SAMPLES_TO_READ);
-
-		float adc_voltage_rms = 0;
-		for(int i=0; i<buff32_len; i++){
-			// Extract only the needed bits.
-			buff32_int[i] = (buff32_int[i] >> 16) & 0xFFF;
-
-			// Running mean.
-			dc_offset = dc_offset + ((float) buff32_int[i] - dc_offset) / buff32_len;
-
-			// Data extraction.
-			float adc_voltage = (buff32_int[i] - round(dc_offset)) * ADC_VAL_TO_VOLTS;
-			float mic_voltage = adc_voltage / PREAMP_GAIN;
-
-			// Needed for the FFT.
-			// From now, buff32_float contains the SPL values [20uPa, 30Pa].
-			buff32_float[i] = round(20 * log10(mic_voltage / MIC_DB_SPL_CONVERSION));
-
-			// Needed for the other values.
-			adc_voltage_rms += pow(adc_voltage, 2);
-		}
-
-		// Aggregate data.
-		adc_voltage_rms = sqrt(adc_voltage_rms / buff32_len);
-		float mic_voltage_rms = adc_voltage_rms / PREAMP_GAIN;
-		int16_t db_spl = round(20 * log10(mic_voltage_rms / MIC_DB_SPL_CONVERSION));
-
-		Serial.printf("buff32_len: %d\n", buff32_len);
-		Serial.printf("dc_offset: %f\n", dc_offset);
-		Serial.printf("adc_voltage_rms_mV: %f\n", adc_voltage_rms * 1000);
-		Serial.printf("mic_voltage_rms_mV: %f\n", mic_voltage_rms * 1000);
-		Serial.printf("db_spl: %d\n", db_spl);
-		Serial.println();
-
-		vTaskDelay(SENDING_DATA_PERIOD_MS / portTICK_PERIOD_MS);
+		sound.evaluate();
+		delay(SENDING_DATA_PERIOD_MS / 4);
 	}
 }
 
 void main_thread(void *parameters){
 	Serial.println("main_thread");
 
+	AudioProcessor_data_t data;
 	while(true){
-		vTaskDelay(1000 / portTICK_PERIOD_MS);
+		data = sound.get();
+
+		Serial.printf("dc_offset: %d\n", data.sample_dc_offset);
+		Serial.printf("adc_voltage_rms_mV: %f\n", data.adc_voltage_rms * 1000);
+		Serial.printf("mic_voltage_rms_mV: %f\n", data.mic_voltage_rms * 1000);
+		Serial.printf("db_spl: %d\n", data.db_spl);
+		Serial.println();
+
+		delay(SENDING_DATA_PERIOD_MS);
 	}
 }
